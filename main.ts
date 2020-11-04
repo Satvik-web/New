f6bcb0b6b97d0cf445e055b2fe366b2d5a2d165b
Satvik-web <72124172+Satvik-web@users.noreply.github.com>
2020-11-04 11:24:21 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.calibrateCompass()
+basic.forever(function () {
+    if (input.compassHeading() < 45) {
+        basic.showArrow(ArrowNames.NorthEast)
+    }
+    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
+        basic.showArrow(ArrowNames.SouthEast)
+    }
+    if (input.compassHeading() > 135 && input.compassHeading() < 225) {
+        basic.showArrow(ArrowNames.SouthWest)
+    }
+    if (input.compassHeading() > 225 && input.compassHeading() < 315) {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+})
